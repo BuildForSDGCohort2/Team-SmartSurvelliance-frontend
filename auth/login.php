a4c072254e316b816f5006d5f21a3032ac188abb
kings101 <okekekingsley101@gmail.com>
2020-10-09 09:37:18 +0100

    Modified

--- auth/login.php
+++ auth/login.php
@@ -6,6 +6,12 @@
     use Aws\CognitoIdentityProvider\CognitoIdentityProviderClient;
     use Aws\CognitoIdentity\CognitoIdentityClient;
     use Aws\Sts\StsClient;
+    putenv('CLIENT_ID=73nkbeiki4s2q5c2v9v8ek4aue');
+    putenv('USERPOOL_ID=us-east-1_yXMlljTfq');
+    putenv('AWS_ACCESS_KEY_ID=AKIATBV3IPRIFFNLSWMP');
+    putenv('AWS_SECRET_ACCESS_KEY=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');
+    putenv('REGION=us-east-1');
+    putenv('VERSION=latest');
 
     $wrapper = new AWSCognitoWrapper();
     $wrapper->initialize();
