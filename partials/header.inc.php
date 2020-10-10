6d7fdde2f25ce88ec8e10a5923a0bfad5eddf690
kings101 <okekekingsley101@gmail.com>
2020-10-10 21:46:47 +0100

    Modified

--- partials/header.inc.php
+++ partials/header.inc.php
@@ -11,10 +11,22 @@
     
     $entercode = false; 
 
-    if(!isset($_GET["id_token"]) && !isset($_GET['access_token'])){
-        // header('Location: ./auth/404.php');
-        $entercode = false;
-    }else{
+    if(!isset($_GET["id_token"]) && !isset($_GET['access_token'])){ ?>
+        <script>
+            var url_str = window.location.href;
+            //On successful authentication, AWS Cognito will redirect to Call-back URL and pass the access_token as a request parameter. 
+            //If you notice the URL, a “#” symbol is used to separate the query parameters instead of the “?” symbol. 
+            //So we need to replace the “#” with “?” in the URL and call the page again.
+            
+            if(url_str.includes("#")){
+                var url_str_hash_replaced = url_str.replace("#", "?");
+                window.location.href = url_str_hash_replaced;
+            }
+            
+        </script>
+
+    <?php }else{
+
         $entercode = true;
         $id_token = $_GET["id_token"];
         $access_token = $_GET["access_token"];
