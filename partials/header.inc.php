4c822871ba3ac650428ca89a0c7cf1f0a8beac00
kings101 <okekekingsley101@gmail.com>
2020-10-12 01:29:40 +0100

    'modified'

--- partials/header.inc.php
+++ partials/header.inc.php
@@ -62,15 +62,12 @@
         <link href="https://vjs.zencdn.net/7.8.4/video-js.css" rel="stylesheet" />
         <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 
-        <!-- Firebase SDK for web -->
-        <!-- The core Firebase JS SDK is always required and must be listed first -->
-        <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js"></script>
-
-        <!-- TODO: Add SDKs for Firebase products that you want to use
-             https://firebase.google.com/docs/web/setup#available-libraries -->
-        <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-analytics.js"></script>
-
-      
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body>
         <div class="loader">
@@ -197,6 +194,7 @@
                         // alert('Hey');
                         showNotification();
                         // setInterval(function(){ showNotification(); }, 15000);
-                    }   
+                    })
+                    
                 });
             </script>
