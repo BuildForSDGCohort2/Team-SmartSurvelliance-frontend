58426f4210780fb5068f160832df91041601c0cb
kings101 <okekekingsley101@gmail.com>
2020-10-12 01:33:37 +0100

    'moidifed'

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-    header("Location: ./auth/login.php");
+    header("Location: https://kingso101-smart-home-demo.auth.us-east-1.amazoncognito.com/login?client_id=73nkbeiki4s2q5c2v9v8ek4aue&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://smart-surveillance-web-app.herokuapp.com/process.php");
+    // header("Location: ./auth/login.php");
 ?>
