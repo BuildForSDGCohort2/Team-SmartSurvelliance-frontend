2809e45b48af131728092d3b4f4883f54f226a6b
kings101 <okekekingsley101@gmail.com>
2020-10-13 23:13:09 +0100

    Modified

--- send.php
+++ send.php
@@ -1,57 +1,36 @@
 <?php 
-	define('SERVER_API_KEY', 'AAAAjfN0wn4:APA91bEKcCeOfFpeNLaIgU032GFbfVVuhES_zZVuTuOxTRIajwJsHnhvccESa3WSkkhzYLg0w65D2po6iIlhwJsIQ3fBcjyz_j5CgNSJrGAR8kxd2fhxCsUcOPjp5gyDhla4oFvj_k_g');
 
-	// require 'DbConnect.php';
-	// $db = new DbConnect;
-	// $conn = $db->connect();
-	// $stmt = $conn->prepare('SELECT * FROM tokens');
-	// $stmt->execute();
-	// $tokens = $stmt->fetchAll(PDO::FETCH_ASSOC);
+function sendPushNotification($fields = array())
+{
+    $API_ACCESS_KEY = 'AAAAjfN0wn4:APA91bEKcCeOfFpeNLaIgU032GFbfVVuhES_zZVuTuOxTRIajwJsHnhvccESa3WSkkhzYLg0w65D2po6iIlhwJsIQ3fBcjyz_j5CgNSJrGAR8kxd2fhxCsUcOPjp5gyDhla4oFvj_k_g';
+    $headers = array
+    (
+        'Authorization: key=' . $API_ACCESS_KEY,
+        'Content-Type: application/json'
+    );
+    $ch = curl_init();
+    curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+    curl_setopt( $ch,CURLOPT_POST, true );
+    curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+    curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+    curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+    $result = curl_exec($ch );
+    curl_close( $ch );
+    return $result;
+}
 
-
-	$tokens = 'cZLSkPkml3gjya_qLA1EvT:APA91bH-IQnnUlX7Neb6faoBNFjvYzbXLqH0uQT_puLQx1czwe7kWLO6qFkGRK7si1ZDOoAurqDNXdmwT9NFJupAxBuJTjJfTNANHCWI61uAf7yJ6rUOXcL0zz2GllFbznwpDfFopoXe';
-
-	foreach ($tokens as $token) {
-		$registrationIds[] = $token['token'];
-	}
-
-	// $tokens = ['cCLA1_8Inic:APA91bGhuCksjWEETYWVOh04scsZInxdWmXekEr5F9-1zJuTDZDw3It_tNmpA__PmoxDTISZzplD_ciXvsuw2pMtYSzdfIUAUfcTLnghvJS0CVkYW9sVx2HnF1rqnxsFgSdYmcXpHKLs'];
-	
-	$header = [
-		'Authorization: Key=' . SERVER_API_KEY,
-		'Content-Type: Application/json'
-	];
-
-	$msg = [
-		'title' => 'ALERT! YOU HAVE AN INTRUDER',
-		'body' => 'You have someone at the front door.',
-		'icon' => 'https://smart-surveillance-web-app.herokuapp.com/assets/images/logo.jpg',
-		'image' => 'https://smart-surveillance-web-app.herokuapp.com/assets/images/logo.jpg',
-	];
-
-	$payload = [
-		'registration_ids' 	=> $registrationIds,
-		'data'				=> $msg
-	];
-
-	$curl = curl_init();
-
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_CUSTOMREQUEST => "POST",
-	  CURLOPT_POSTFIELDS => json_encode( $payload ),
-	  CURLOPT_HTTPHEADER => $header
-	));
-
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-
-	curl_close($curl);
-
-	if ($err) {
-	  echo "cURL Error #:" . $err;
-	} else {
-	  echo $response;
-	}
- ?>
+$title = 'ALERT! YOU HAVE AN INTRUDER';
+$message = 'You have someone at the front door.';
+$fields = array
+(
+    'registration_ids'  => ['cZLSkPkml3gjya_qLA1EvT:APA91bF3vRvEQ1EbpSjkoUbQ8bTfwAE5dSYXpBBKiLXvhg1avym514593GGzK46yx_kiL7I9b15Ir4iaCJmx0UqNbEiiCes8YLPerOnLwU-gZJ7dSS7k8i7HaRk7rxyRBd2tjd4ciCdo'],
+    'data'          => '',
+    'priority' => 'high',
+    'notification' => array(
+        'body' => $message,
+        'title' => $title,
+        'sound' => 'default',
+        'icon' => 'https://smart-surveillance-web-app.herokuapp.com/assets/images/logo.jpg'
+    )
+);
