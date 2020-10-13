f1d083e46646c2fa049959138192a9b4713af8f1
kings101 <okekekingsley101@gmail.com>
2020-10-13 22:46:41 +0100

    'Modified'

--- send.php
+++ send.php
@@ -18,7 +18,7 @@
 	$registrationIds = 'cZLSkPkml3gjya_qLA1EvT:APA91bH-IQnnUlX7Neb6faoBNFjvYzbXLqH0uQT_puLQx1czwe7kWLO6qFkGRK7si1ZDOoAurqDNXdmwT9NFJupAxBuJTjJfTNANHCWI61uAf7yJ6rUOXcL0zz2GllFbznwpDfFopoXe';
 	
 	$header = [
-		'Authorization: Key=AIzaSyB3YnQ2WaEQWGspxexCKSBc-1dF4kX7lQE',
+		'Authorization: Key=AAAAjfN0wn4:APA91bEKcCeOfFpeNLaIgU032GFbfVVuhES_zZVuTuOxTRIajwJsHnhvccESa3WSkkhzYLg0w65D2po6iIlhwJsIQ3fBcjyz_j5CgNSJrGAR8kxd2fhxCsUcOPjp5gyDhla4oFvj_k_g',
 		'Content-Type: Application/json'
 	];
 
