86f67928c3309b437feaa843f3294330e13628e9
kings101 <okekekingsley101@gmail.com>
2020-10-13 22:44:10 +0100

    'Modified'

--- send.php
+++ send.php
@@ -1,49 +1,57 @@
 <?php 
-	function push_notification_android($device_id,$message){
+	define('SERVER_API_KEY', 'AAAAjfN0wn4:APA91bEKcCeOfFpeNLaIgU032GFbfVVuhES_zZVuTuOxTRIajwJsHnhvccESa3WSkkhzYLg0w65D2po6iIlhwJsIQ3fBcjyz_j5CgNSJrGAR8kxd2fhxCsUcOPjp5gyDhla4oFvj_k_g');
+	$config = require './config/config.php';
 
-    //API URL of FCM
-    $url = 'https://fcm.googleapis.com/fcm/send';
+	// require 'DbConnect.php';
+	// $db = new DbConnect;
+	// $conn = $db->connect();
+	// $stmt = $conn->prepare('SELECT * FROM tokens');
+	// $stmt->execute();
+	// $tokens = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    /*api_key available in:
-    Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key*/    $api_key = 'AAAARjZ7jhU:APA91bFkHbhG5rChfxO0gt9gWfRk57PT0LJ659Yp9rBMC2u4kGrBEN_rnvau_PMu3kh-wvHO0OxQoX_Pa4JxqWNkiG3trsQ_7IIFjV3nhFIwULd3jHUQkQsLO0jTOZmn0zHx6rii6DqP';
+	// foreach ($tokens as $token) {
+	// 	$registrationIds[] = $token['token'];
+	// }
 
-    $message = [
+	// $tokens = ['cCLA1_8Inic:APA91bGhuCksjWEETYWVOh04scsZInxdWmXekEr5F9-1zJuTDZDw3It_tNmpA__PmoxDTISZzplD_ciXvsuw2pMtYSzdfIUAUfcTLnghvJS0CVkYW9sVx2HnF1rqnxsFgSdYmcXpHKLs'];
+
+	$registrationIds = 'cZLSkPkml3gjya_qLA1EvT:APA91bH-IQnnUlX7Neb6faoBNFjvYzbXLqH0uQT_puLQx1czwe7kWLO6qFkGRK7si1ZDOoAurqDNXdmwT9NFJupAxBuJTjJfTNANHCWI61uAf7yJ6rUOXcL0zz2GllFbznwpDfFopoXe';
+	
+	$header = [
+		'Authorization: Key=AIzaSyB3YnQ2WaEQWGspxexCKSBc-1dF4kX7lQE',
+		'Content-Type: Application/json'
+	];
+
+	$msg = [
 		'title' => 'ALERT! YOU HAVE AN INTRUDER',
 		'body' => 'You have someone at the front door.',
 		'icon' => 'https://smart-surveillance-web-app.herokuapp.com/assets/images/logo.jpg',
 		'image' => 'https://smart-surveillance-web-app.herokuapp.com/assets/images/logo.jpg',
 	];
-            
-    $fields = array (
-        'registration_ids' => array (
-                $device_id
-        ),
-        'data' => array (
-                "message" => $message
-        )
-    );
 
-    //header includes Content type and api key
-    $headers = array(
-        'Content-Type:application/json',
-        'Authorization:key='.$api_key
-    );
-                
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-    $result = curl_exec($ch);
-    if ($result === FALSE) {
-    	echo "cURL Error #:" . curl_error($ch));
-    }else {
-	  echo $result;
+	$payload = [
+		'registration_ids' 	=> $registrationIds,
+		'data'				=> $msg
+	];
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS => json_encode( $payload ),
+	  CURLOPT_HTTPHEADER => $header
+	));
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+	  echo "cURL Error #:" . $err;
+	} else {
+	  echo $response;
 	}
-    curl_close($ch);
-    return $result;
-}
  ?>
